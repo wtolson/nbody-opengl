5c7838c6572b20e98721d313f69cb65f71adc874
William Trevor Olson <wtolson@gmail.com>
2016-01-12 12:54:25 -0700

    Use same syntax for debug logging.

--- src/log.h
+++ src/log.h
@@ -8,7 +8,7 @@
 #ifdef NDEBUG
 #define log_debug(M, ...)
 #else
-#define log_debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_debug(M, ...) fprintf(stderr, "[DEBUG] %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
